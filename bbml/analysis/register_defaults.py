a8754a990cb288d82aa9f3b4b0ddd613c4d9c950
Rowan Bradbury <rowanbradbury0@gmail.com>
2026-03-18 12:51:14 +0000

    llama

--- bbml/analysis/register_defaults.py
+++ bbml/analysis/register_defaults.py
@@ -1,7 +1,9 @@
 from bbml.registries import WeightExtractorRegistry, MetricRegistry
 from bbml.analysis.extractors.gpt2 import GPT2WeightExtractor
+from bbml.analysis.extractors.llama import LlamaWeightExtractor
 from bbml.analysis.metrics.cosine import CosineMetric
 
 
 WeightExtractorRegistry.register("gpt2")(GPT2WeightExtractor)
+WeightExtractorRegistry.register("llama")(LlamaWeightExtractor)
 MetricRegistry.register("cosine")(CosineMetric)
